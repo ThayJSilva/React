4edf9ac0c1870835e0810a8e9845d89dfdcf1c6b
unknown <thayjsilva@gmail.com>
2022-11-25 12:46:14 -0300

    atualização

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Grid } from '@material-ui/core';
 import Home from './paginas/home/Home';
 import Navbar from './navbar/Navbar';
 import Footer from './estaticos/footer/Footer';
@@ -8,11 +10,22 @@ import Footer from './estaticos/footer/Footer';
 function App() {
   return (
     <>
-    <Navbar />
-    <Home />
-    <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+
+          <Route path='/home' element={<Home />} />
+
+        </Routes>
+
+        <Footer />
+
+      </Router>
+
+
+
     </>
-    
+
   );
 }
 
